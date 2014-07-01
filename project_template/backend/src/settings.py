fac2bfdbaf28aec23903638119312665df148951
renzon <renzon@gmail.com>
2014-07-01 00:46:15 -0300

    close #7

--- project_template/backend/src/settings.py
+++ project_template/backend/src/settings.py
@@ -1,22 +1,26 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
+from config.tmpl_middleware import TemplateMiddleware, TemplateWriteMiddleware
 from gaecookie.middleware import CSRFMiddleware, CSRFInputToDependency
-from tmpl_middleware import TemplateMiddleware
+from tekton.gae.middleware.json_middleware import JsonResponseMiddleware
+from tekton.gae.middleware.redirect import RedirectMiddleware
 from tekton.gae.middleware.email_errors import EmailMiddleware
-from tekton.gae.middleware.json_middleware import JsonMiddleare
 from tekton.gae.middleware.parameter import RequestParamsMiddleware
 from tekton.gae.middleware.router_middleware import RouterMiddleware, ExecutionMiddleware
 from tekton.gae.middleware.webapp2_dependencies import Webapp2Dependencies
 
 SENDER_EMAIL = 'renzon@gmail.com'
-WEB_BASE_PACKAGE = "web"
+TEMPLATE_404_ERROR = 'base/404.html'
+TEMPLATE_400_ERROR = 'base/400.html'
 MIDDLEWARES = [TemplateMiddleware,
-               JsonMiddleare,
                EmailMiddleware,
                Webapp2Dependencies,
                RequestParamsMiddleware,
                CSRFInputToDependency,
                RouterMiddleware,
                CSRFMiddleware,
-               ExecutionMiddleware]
+               ExecutionMiddleware,
+               TemplateWriteMiddleware,
+               JsonResponseMiddleware,
+               RedirectMiddleware]
 
